b12f85b8755a23835c924c3ff272f69ceae20e55
exhesham.com <exhesham@gmail.com>
2021-08-17 10:54:28 +0300

    Update markdown_toc.py

--- markdown_toc.py
+++ markdown_toc.py
@@ -4,11 +4,13 @@ parser = argparse.ArgumentParser(description='generate table of contents for md 
 parser.add_argument('--file', help='The file to run the markdown on',  default="README.md", nargs=1, required=True)
 args = parser.parse_known_args()
 filename = args[0].file[0]
-with open(filename) as f:
-    for line in f:
+with open(filename, encoding="utf8") as f:
+    
+    data = f.read().split('\n')
+    for line in data:
         if re.match(r'^#+ ', line):
             title = re.sub('#','',line).strip()
             hash = title.lower().replace(' ','-')
             manipulated_line = '**[%s](#%s)**' % (title,hash)
             tabs = re.sub('#','\t',line.strip()[:line.strip().index(' ')+1])
-            print tabs+ '* ' + manipulated_line
+            print (tabs+ '* ' + manipulated_line)
